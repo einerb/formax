5ff6aa1529afefd70e77ea6aa46ad4c2eb2bff19
Einer Bravo <einerbravo@gmail.com>
2020-06-25 13:39:15 -0500

    Create models.

---
+++ app/Order.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    protected $fillable = [
+        'id',
+        'channel',
+        'state',
+        'value',
+        'discount',
+        'delivery',
+        'dispatch',
+        'items',
+    ];
+}
