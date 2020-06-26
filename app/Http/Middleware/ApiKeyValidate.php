e525806cee28f56b00739e19a751c09d32c4bb00
Einer Bravo <einerbravo@gmail.com>
2020-06-26 12:51:42 -0500

    Add Api Key for route security.

---
+++ app/Http/Middleware/ApiKeyValidate.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class ApiKeyValidate
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if (!$request->has("api_key")) {
+            return response()->json([
+              'status' => 401,
+              'message' => 'Unauthorized access!',
+            ], 401);
+        }
+      
+        if ($request->has("api_key")) {
+            $api_key = "TeshOu4i2WUDGa";
+            if ($request->input("api_key") != $api_key) {
+                return response()->json([
+                'status' => 401,
+                'message' => 'Unauthorized access!',
+              ], 401);
+            }
+        }
+      
+        return $next($request);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -18,20 +18,23 @@ use Illuminate\Support\Facades\Route;
     return $request->user();
 }); */
 
-// GET
-Route::get('products', 'ProductController@index');
-Route::get('orders', 'OrderController@index');
-Route::get('products/{id}', 'ProductController@show');
-Route::get('orders/{id}', 'OrderController@show');
 
-// POST
-Route::post('products', 'ProductController@store');
-Route::post('orders', 'OrderController@store');
+Route::group(["middleware"=> "apikey.validate"], function () {
+    // GET
+    Route::get('products', 'ProductController@index');
+    Route::get('orders', 'OrderController@index');
+    Route::get('products/{id}', 'ProductController@show');
+    Route::get('orders/{id}', 'OrderController@show');
 
-// PUT
-Route::put('products/update/{id}', 'ProductController@update');
-Route::put('orders/update/{id}', 'OrderController@update');
+    // POST
+    Route::post('products', 'ProductController@store');
+    Route::post('orders', 'OrderController@store');
 
-// DELETE
-Route::delete('products/remove/{id}', 'ProductController@destroy');
-Route::delete('orders/remove/{id}', 'OrderController@destroy');
+    // PUT
+    Route::put('products/update/{id}', 'ProductController@update');
+    Route::put('orders/update/{id}', 'OrderController@update');
+
+    // DELETE
+    Route::delete('products/remove/{id}', 'ProductController@destroy');
+    Route::delete('orders/remove/{id}', 'OrderController@destroy');
+});
